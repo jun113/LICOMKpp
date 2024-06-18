86a1184c6909fff3e452c6f462a93a94eafcdc5d
Junlin Wei <weijunlin@cnic.cn>
2024-06-18 22:48:18 +0800

    bugs fixes in jra daily at high resolution

--- src/kokkos-impl/kokkos_jra_daily.cpp
+++ src/kokkos-impl/kokkos_jra_daily.cpp
@@ -144,10 +144,6 @@ void kokkos_init_jra_daily_high() {
   for (int i = 0; i < CppPconstMod::mon0 - 1; ++i) {
     mon_day += CppPconstMod::nmonth[i];
   }
-  int irec = mon_day + CppPconstMod::iday;
-  if (CppParamMod::mytid == 0) {
-  printf("jra daily start irec = %d\n", irec);
-  }
 
 #ifdef KOKKOS_ENABLE_DEVICE_MEM_SPACE
   CppForcMod::tsa3    = new double [JMT * IMT];
@@ -174,68 +170,68 @@ void kokkos_init_jra_daily_high() {
     ViewFloat3D::HostMirror h_v_rf = create_mirror_view(*p_v_rf);
     ViewDouble1D::HostMirror h_v_buffer = create_mirror_view(*p_v_buffer);
     // update s_lon s_lat
-    read_jra(irec, 365, "runoff_all.2016_daily.nc", h_v_buffer.data(), 
+    read_jra(1, 365, "runoff_all.2016_daily.nc", h_v_buffer.data(), 
         h_v_s_lon.data(), h_v_s_lat.data(), h_v_rf.data());
     Kokkos::deep_copy(*p_v_rf, h_v_rf);
   } else if (CppParamMod::mytid == 1) {
       p_v_t10 = (ViewFloat3D *) malloc(sizeof(ViewFloat3D));
       new (p_v_t10) ViewFloat3D("pointer_view_t10", 365, S_JMT, S_IMT);
       ViewFloat3D::HostMirror h_v_t10 = create_mirror_view(*p_v_t10);
-    read_jra(irec, 365, "t_10.2016_daily.nc", h_v_t10.data());
+    read_jra(1, 365, "t_10.2016_daily.nc", h_v_t10.data());
     Kokkos::deep_copy(*p_v_t10, h_v_t10);
   } else if (CppParamMod::mytid == 2) {
       p_v_u10 = (ViewFloat3D *) malloc(sizeof(ViewFloat3D));
       new (p_v_u10) ViewFloat3D("pointer_view_u10", 365, S_JMT, S_IMT);
       ViewFloat3D::HostMirror h_v_u10 = create_mirror_view(*p_v_u10);
-    read_jra(irec, 365, "u_10.2016_daily.nc", h_v_u10.data());
+    read_jra(1, 365, "u_10.2016_daily.nc", h_v_u10.data());
     Kokkos::deep_copy(*p_v_u10, h_v_u10);
   } else if (CppParamMod::mytid == 3) {
       p_v_v10 = (ViewFloat3D *) malloc(sizeof(ViewFloat3D));
       new (p_v_v10) ViewFloat3D("pointer_view_v10", 365, S_JMT, S_IMT);
       ViewFloat3D::HostMirror h_v_v10 = create_mirror_view(*p_v_v10);
-    read_jra(irec, 365, "v_10.2016_daily.nc", h_v_v10.data());
+    read_jra(1, 365, "v_10.2016_daily.nc", h_v_v10.data());
     Kokkos::deep_copy(*p_v_v10, h_v_v10);
   } else if (CppParamMod::mytid == 4) {
       p_v_slp = (ViewFloat3D *) malloc(sizeof(ViewFloat3D));
       new (p_v_slp) ViewFloat3D("pointer_view_slp", 365, S_JMT, S_IMT);
       ViewFloat3D::HostMirror h_v_slp = create_mirror_view(*p_v_slp);
-    read_jra(irec, 365, "slp.2016_daily.nc", h_v_slp.data());
+    read_jra(1, 365, "slp.2016_daily.nc", h_v_slp.data());
     Kokkos::deep_copy(*p_v_slp, h_v_slp);
   } else if (CppParamMod::mytid == 5) {
       p_v_q10 = (ViewFloat3D *) malloc(sizeof(ViewFloat3D));
       new (p_v_q10) ViewFloat3D("pointer_view_q10", 365, S_JMT, S_IMT);
       ViewFloat3D::HostMirror h_v_q10 = create_mirror_view(*p_v_q10);
-    read_jra(irec, 365, "q_10.2016_daily.nc", h_v_q10.data());
+    read_jra(1, 365, "q_10.2016_daily.nc", h_v_q10.data());
     Kokkos::deep_copy(*p_v_q10, h_v_q10);
   } else if (CppParamMod::mytid == 6) {
       p_v_swhf = (ViewFloat3D *) malloc(sizeof(ViewFloat3D));
       new (p_v_swhf) ViewFloat3D("pointer_view_swhf", 365, S_JMT, S_IMT);
       ViewFloat3D::HostMirror h_v_swhf = create_mirror_view(*p_v_swhf);
-    read_jra(irec, 365, "rsds.2016_daily.nc", h_v_swhf.data());
+    read_jra(1, 365, "rsds.2016_daily.nc", h_v_swhf.data());
     Kokkos::deep_copy(*p_v_swhf, h_v_swhf);
   } else if (CppParamMod::mytid == 7) {
       p_v_lwhf = (ViewFloat3D *) malloc(sizeof(ViewFloat3D));
       new (p_v_lwhf) ViewFloat3D("pointer_view_lwhf", 365, S_JMT, S_IMT);
       ViewFloat3D::HostMirror h_v_lwhf = create_mirror_view(*p_v_lwhf);
-    read_jra(irec, 365, "rlds.2016_daily.nc", h_v_lwhf.data());
+    read_jra(1, 365, "rlds.2016_daily.nc", h_v_lwhf.data());
     Kokkos::deep_copy(*p_v_lwhf, h_v_lwhf);
   } else if (CppParamMod::mytid == 8) {
       p_v_precr = (ViewFloat3D *) malloc(sizeof(ViewFloat3D));
       new (p_v_precr) ViewFloat3D("pointer_view_precr", 365, S_JMT, S_IMT);
       ViewFloat3D::HostMirror h_v_precr = create_mirror_view(*p_v_precr);
-    read_jra(irec, 365, "rain.2016_daily.nc", h_v_precr.data());
+    read_jra(1, 365, "rain.2016_daily.nc", h_v_precr.data());
     Kokkos::deep_copy(*p_v_precr, h_v_precr);
   } else if (CppParamMod::mytid == 9) {
       p_v_precs = (ViewFloat3D *) malloc(sizeof(ViewFloat3D));
       new (p_v_precs) ViewFloat3D("pointer_view_precs", 365, S_JMT, S_IMT);
       ViewFloat3D::HostMirror h_v_precs = create_mirror_view(*p_v_precs);
-    read_jra(irec, 365, "snow.2016_daily.nc", h_v_precs.data());
+    read_jra(1, 365, "snow.2016_daily.nc", h_v_precs.data());
     Kokkos::deep_copy(*p_v_precs, h_v_precs);
   } else if (CppParamMod::mytid == 10) {
       p_v_si = (ViewFloat3D *) malloc(sizeof(ViewFloat3D));
       new (p_v_si) ViewFloat3D("pointer_view_si", 365, S_JMT, S_IMT);
       ViewFloat3D::HostMirror h_v_si = create_mirror_view(*p_v_si);
-    read_jra(irec, 365, "ice.2016_daily.nc", h_v_si.data());
+    read_jra(1, 365, "ice.2016_daily.nc", h_v_si.data());
     Kokkos::deep_copy(*p_v_si, h_v_si);
   }
 #else
@@ -244,58 +240,58 @@ void kokkos_init_jra_daily_high() {
     CppForcMod::rf = new float[365 * S_IMT * S_JMT];
     new (p_v_rf) ViewFloat3D(CppForcMod::rf, 365, S_JMT, S_IMT);
     // update s_lon s_lat
-    read_jra(irec, 365, "runoff_all.2016_daily.nc", CppForcMod::buffer, 
+    read_jra(1, 365, "runoff_all.2016_daily.nc", CppForcMod::buffer, 
         (*p_v_s_lon).data(), (*p_v_s_lat).data(), (*p_v_rf).data());
   } else if (CppParamMod::mytid == 1) {
     p_v_t10 = (ViewFloat3D *) malloc (sizeof (ViewFloat3D));
     CppForcMod::t10 = new float[365 * S_IMT * S_JMT];
     new (p_v_t10) ViewFloat3D(CppForcMod::t10, 365, S_JMT, S_IMT);
-    read_jra(irec, 365, "t_10.2016_daily.nc", (*p_v_t10).data());
+    read_jra(1, 365, "t_10.2016_daily.nc", (*p_v_t10).data());
   } else if (CppParamMod::mytid == 2) {
     p_v_u10 = (ViewFloat3D *) malloc (sizeof (ViewFloat3D));
     CppForcMod::u10 = new float[365 * S_IMT * S_JMT];
     new (p_v_u10) ViewFloat3D(CppForcMod::u10, 365, S_JMT, S_IMT);
-    read_jra(irec, 365, "u_10.2016_daily.nc", (*p_v_u10).data());
+    read_jra(1, 365, "u_10.2016_daily.nc", (*p_v_u10).data());
   } else if (CppParamMod::mytid == 3) {
     p_v_v10 = (ViewFloat3D *) malloc (sizeof (ViewFloat3D));
     CppForcMod::v10 = new float[365 * S_IMT * S_JMT];
     new (p_v_v10) ViewFloat3D(CppForcMod::v10, 365, S_JMT, S_IMT);
-    read_jra(irec, 365, "v_10.2016_daily.nc", (*p_v_v10).data());
+    read_jra(1, 365, "v_10.2016_daily.nc", (*p_v_v10).data());
   } else if (CppParamMod::mytid == 4) {
     p_v_slp = (ViewFloat3D *) malloc (sizeof (ViewFloat3D));
     CppForcMod::slp = new float[365 * S_IMT * S_JMT];
     new (p_v_slp) ViewFloat3D(CppForcMod::slp, 365, S_JMT, S_IMT);
-    read_jra(irec, 365, "slp.2016_daily.nc", (*p_v_slp).data());
+    read_jra(1, 365, "slp.2016_daily.nc", (*p_v_slp).data());
   } else if (CppParamMod::mytid == 5) {
     p_v_q10 = (ViewFloat3D *) malloc (sizeof (ViewFloat3D));
     CppForcMod::q10 = new float[365 * S_IMT * S_JMT];
     new (p_v_q10) ViewFloat3D(CppForcMod::q10, 365, S_JMT, S_IMT);
-    read_jra(irec, 365, "q_10.2016_daily.nc", (*p_v_q10).data());
+    read_jra(1, 365, "q_10.2016_daily.nc", (*p_v_q10).data());
   } else if (CppParamMod::mytid == 6) {
     p_v_swhf = (ViewFloat3D *) malloc (sizeof (ViewFloat3D));
     CppForcMod::swhf = new float[365 * S_IMT * S_JMT];
     new (p_v_swhf) ViewFloat3D(CppForcMod::swhf, 365, S_JMT, S_IMT);
-    read_jra(irec, 365, "rsds.2016_daily.nc", (*p_v_swhf).data());
+    read_jra(1, 365, "rsds.2016_daily.nc", (*p_v_swhf).data());
   } else if (CppParamMod::mytid == 7) {
     p_v_lwhf = (ViewFloat3D *) malloc (sizeof (ViewFloat3D));
     CppForcMod::lwhf = new float[365 * S_IMT * S_JMT];
     new (p_v_lwhf) ViewFloat3D(CppForcMod::lwhf, 365, S_JMT, S_IMT);
-    read_jra(irec, 365, "rlds.2016_daily.nc", (*p_v_lwhf).data());
+    read_jra(1, 365, "rlds.2016_daily.nc", (*p_v_lwhf).data());
   } else if (CppParamMod::mytid == 8) {
     p_v_precr = (ViewFloat3D *) malloc (sizeof (ViewFloat3D));
     CppForcMod::precr = new float[365 * S_IMT * S_JMT];
     new (p_v_precr) ViewFloat3D(CppForcMod::precr, 365, S_JMT, S_IMT);
-    read_jra(irec, 365, "rain.2016_daily.nc", (*p_v_precr).data());
+    read_jra(1, 365, "rain.2016_daily.nc", (*p_v_precr).data());
   } else if (CppParamMod::mytid == 9) {
     p_v_precs = (ViewFloat3D *) malloc (sizeof (ViewFloat3D));
     CppForcMod::precs = new float[365 * S_IMT * S_JMT];
     new (p_v_precs) ViewFloat3D(CppForcMod::precs, 365, S_JMT, S_IMT);
-    read_jra(irec, 365, "snow.2016_daily.nc", (*p_v_precs).data());
+    read_jra(1, 365, "snow.2016_daily.nc", (*p_v_precs).data());
   } else if (CppParamMod::mytid == 10) {
     p_v_si = (ViewFloat3D *) malloc (sizeof (ViewFloat3D));
     CppForcMod::si = new float[365 * S_IMT * S_JMT];
     new (p_v_si) ViewFloat3D(CppForcMod::si, 365, S_JMT, S_IMT);
-    read_jra(irec, 365, "ice.2016_daily.nc", (*p_v_si).data());
+    read_jra(1, 365, "ice.2016_daily.nc", (*p_v_si).data());
   }
 #endif
   // Broadcasting the s_lon and s_lat
